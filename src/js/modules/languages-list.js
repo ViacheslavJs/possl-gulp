eb45ce533d90a34088bd4ec7b54b4e051e83719b
ViacheslavJs <cherniienko7@gmail.com>
2023-10-24 20:57:13 +0300

    added animation, etc.

--- src/js/modules/languages-list.js
+++ src/js/modules/languages-list.js
@@ -7,16 +7,20 @@ export const languagesList = () => {
 
     s1.addEventListener('click', () => {
         isDropdownVisible = !isDropdownVisible;
-        s2.style.display = isDropdownVisible ? 'block' : 'none';
+        //s2.style.display = isDropdownVisible ? 'block' : 'none';
 
         // Toggle the Font Awesome icon class
         if (isDropdownVisible) {
+            s2.style.maxHeight = s2.scrollHeight + 'px';    
             arrowIcon.classList.remove('fa-chevron-down');
             arrowIcon.classList.add('fa-chevron-up');
         } else {
+            s2.style.maxHeight = '0'; 
             arrowIcon.classList.remove('fa-chevron-up');
             arrowIcon.classList.add('fa-chevron-down');
         }
     });
 };
+
+
 
